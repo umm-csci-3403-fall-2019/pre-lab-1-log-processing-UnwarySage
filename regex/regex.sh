7e7b6ea76e6fb37557812c5bf808fbd6f8b877b0
unwarysage <unwary@comcast.net>
2019-09-04 23:04:12 -0500

    Finish second regex exercise
    sed discarding non-matches prevents
    awesome from intruding

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+#To filter only lines that we want, and get rid of awesome
+FILTERED_FILE=$(grep -o 'I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+).' r1_input.txt)
+echo $FILTERED_FILE
+
+#https://stackoverflow.com/questions/1665549/have-sed-ignore-non-matching-lines
+sed -E 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2 \2\n/;t;d' < r1_input.txt > r1_output.txt
